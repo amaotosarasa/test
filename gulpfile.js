eb2c497ff2382b662e5e895dd6332b476000dde3
amaotosarasa <amaotosarasa@gmail.com>
2020-12-29 14:59:33 +0900

    gulp-sassの実装

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,17 @@
+'use strict';
+
 var gulp = require('gulp');
 var browsersync = require("browser-sync").create();
+var sass = require('gulp-sass');
+
+sass.compiler = require('node-sass');
+
+// Sassファイル
+gulp.task('sass', function () {
+	return gulp.src('sass/**/*.scss')
+	.pipe(sass().on('error', sass.logError))
+	.pipe(gulp.dest('root/'));
+});
 
 // サーバーを立ち上げる
 gulp.task('build-server', function (done) {
@@ -17,6 +29,7 @@ gulp.task('watch-files', function (done) {
 	gulp.watch("root/**/*.html", gulp.task('browser-reload'));
 	gulp.watch("root/**/*/*.css", gulp.task('browser-reload'));
 	gulp.watch("root/**/*/*.js", gulp.task('browser-reload'));
+	gulp.watch('sass/**/*.scss', gulp.task('sass'));
 	done();
 	console.log(('gulp watch started'));
 });
@@ -29,7 +42,7 @@ gulp.task('browser-reload', function (done) {
 });
 
 // タスクの実行
-gulp.task('default', gulp.series('build-server', 'watch-files', function (done) {
+gulp.task('default', gulp.series('build-server', 'watch-files', 'sass',function (done) {
 	done();
 	console.log('Default all task done!');
 }));

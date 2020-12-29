bc9c952958b49390c66391d3cf3aa0de4e848920
amaotosarasa <amaotosarasa@gmail.com>
2020-12-29 23:55:23 +0900

    css整形タスク追加

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 'use strict';
 
 let gulp = require('gulp');
-let browsersync = require("browser-sync").create();
+let browsersync = require('browser-sync').create();
 let sass = require('gulp-sass');
 let header = require('gulp-header');
 let replace = require('gulp-replace');
+let postcss = require('gulp-postcss');
+let autoprefixer = require('autoprefixer');
+let csscomb = require('gulp-csscomb');
 
 sass.compiler = require('node-sass');
 
@@ -16,6 +19,12 @@ gulp.task('sass', function () {
 		}).on('error', sass.logError))
 		.pipe(replace(/@charset "UTF-8";/g, '')) // charsetの検索
 		.pipe(header('@charset "UTF-8";\n\n'))　// charsetの挿入
+		.pipe(postcss([
+			autoprefixer({
+			cascade: false
+			})
+		]))
+		.pipe(csscomb())
 		.pipe(gulp.dest('root/'));
 });
 
@@ -32,9 +41,9 @@ gulp.task('build-server', function (done) {
 
 // 監視ファイル
 gulp.task('watch-files', function (done) {
-	gulp.watch("root/**/*.html", gulp.task('browser-reload'));
-	gulp.watch("root/**/*/*.css", gulp.task('browser-reload'));
-	gulp.watch("root/**/*/*.js", gulp.task('browser-reload'));
+	gulp.watch('root/**/*.html', gulp.task('browser-reload'));
+	gulp.watch('root/**/*/*.css', gulp.task('browser-reload'));
+	gulp.watch('root/**/*/*.js', gulp.task('browser-reload'));
 	gulp.watch('sass/**/*.scss', gulp.task('sass'));
 	done();
 	console.log(('gulp watch started'));

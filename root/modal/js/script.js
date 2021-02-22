7e47a54790ae822698ab2092cb755c5773196263
amaotosarasa <cock.robin20@gmail.com>
2021-02-22 23:52:26 +0900

    [update] モダール出現時の処理

--- root/modal/js/script.js
+++ root/modal/js/script.js
@@ -1,9 +1,42 @@
 
 (function(){
 	'use strict';
-
-	console.log('ああああ');
+	
+	/**
+	 * モーダル出現ポップアップ時の作成処理
+	 * ClickEle クリックイベントの引数を受け取りsrcとaltを読み込む
+	 */
+	const modal = (ClickEle) =>{
+		const
+			target = ClickEle.target,
+			targetGetAttrSrc = target.getAttribute('src'),
+			targetGetAttrAlt = target.getAttribute('alt'),
+			createEle = document.createElement('div');
+		
+		// モーダル用のDOM要素を作成
+		createEle.classList.add('modal');
+		createEle.innerHTML = 
+			`
+			<div class="modal">
+				<div class="modal__bg">
+					<div class="modal__contents">
+						<span class="modal--close"></span>
+						<div class="modal__img">
+							<img src="${targetGetAttrSrc}" alt="${targetGetAttrAlt}">
+						</div>
+						<p>${targetGetAttrAlt}</p>
+					</div>
+				</div>
+			</div> 
+			`;
+		document.body.appendChild(createEle);
+	};
+	
 	const modaelItem = document.querySelectorAll('.js-modal__item');
-	console.log(modaelItem);
+	modaelItem.forEach(element => {
+		element.addEventListener('click',(e) =>{
+			modal(e);
+		});
+	});
 
 }());

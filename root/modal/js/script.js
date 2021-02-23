40fb9da9d7e2c3fdfa9ffaf78d2518c71b7ef4fa
amaotosarasa <cock.robin20@gmail.com>
2021-02-23 21:39:44 +0900

    [update] JSモーダル削除時の処理追加

--- root/modal/js/script.js
+++ root/modal/js/script.js
@@ -31,12 +31,34 @@
 			`;
 		document.body.appendChild(createEle);
 	};
+
+	/**
+	 * モーダル出現後の処理
+	 * ClickEle クリックイベントの引数を受け取りclassを読み込む
+	 */
+	const removeMmodal = (ClickEle) =>{
+		const target = ClickEle.target;
+
+			target.getAttribute('class');
+			if(target.getAttribute('class') === 'modal__bg' || target.getAttribute('class') === 'modal--close'){
+				// 取得したクラスがmodal__bgもしくはmodal--closeであれば処理を開始
+				// モーダルの削除
+				document.getElementsByClassName('modal')[0].remove();
+			}
+	};
 	
+	// モーダルアイテムのクリック処理
 	const modaelItem = document.querySelectorAll('.js-modal__item');
 	modaelItem.forEach(element => {
 		element.addEventListener('click',(e) =>{
 			modal(e);
 		});
+	});
+
+	// モーダル削除時のクリック処理
+	// 動的処理のDOM要素なので全体にクリック処理をかける。
+	document.addEventListener('click', (e)=>{
+		removeMmodal(e);
 	});
 
 }());

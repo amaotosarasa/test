f15e70f6f83698fd9cb65a3236fc5782bf5958da
amaotosarasa <cock.robin20@gmail.com>
2021-04-11 00:45:15 +0900

    [add] JS追加

---
+++ root/scrollfy/js/script.js
@@ -0,0 +1,8 @@
+$(function () {
+	$.scrollify({
+		section: ".scroll_block",　// 画面幅いっぱいスクロールさせたい要素を指定
+		interstitialSection: ".header", // 画面幅いっぱいスクロールから除外したい要素を指定 header/footerなど
+		scrollbars: false, // スクロールバーを非表示
+		updateHash: false, // URLの末尾に自動で#がつく機能を停止
+	});
+});

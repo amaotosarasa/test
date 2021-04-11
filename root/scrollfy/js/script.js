c0f5b9dfa550ca97fea7917d297bf1fae69534dd
amaotosarasa <cock.robin20@gmail.com>
2021-04-11 20:11:27 +0900

    [updeta] jsの修正 footerのスクロール解除

--- root/scrollfy/js/script.js
+++ root/scrollfy/js/script.js
@@ -1,7 +1,7 @@
 $(function () {
 	$.scrollify({
 		section: ".scroll_block",　// 画面幅いっぱいスクロールさせたい要素を指定
-		interstitialSection: ".header", // 画面幅いっぱいスクロールから除外したい要素を指定 header/footerなど
+		interstitialSection: ".header,.footer", // 画面幅いっぱいスクロールから除外したい要素を指定 header/footerなど
 		scrollbars: false, // スクロールバーを非表示
 		updateHash: false, // URLの末尾に自動で#がつく機能を停止
 	});

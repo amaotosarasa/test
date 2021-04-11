8900788358fd9bfaed2b4cd56d0882bca5259949
amaotosarasa <cock.robin20@gmail.com>
2021-04-11 21:23:57 +0900

    [update] JS調整 ページネーションの用の処理の追加

--- root/scrollfy/js/script.js
+++ root/scrollfy/js/script.js
@@ -1,8 +1,37 @@
 $(function () {
+
+	let current;
 	$.scrollify({
-		section: ".scroll_block",　// 画面幅いっぱいスクロールさせたい要素を指定
-		interstitialSection: ".header,.footer", // 画面幅いっぱいスクロールから除外したい要素を指定 header/footerなど
+		section: '.scroll_block',　// 画面幅いっぱいスクロールさせたい要素を指定
+		interstitialSection: '.header,.footer', // 画面幅いっぱいスクロールから除外したい要素を指定 header/footerなど
 		scrollbars: false, // スクロールバーを非表示
 		updateHash: false, // URLの末尾に自動で#がつく機能を停止
+		setHeights:false, // 高さ調整取り消し
+		before:function(i,panels) {
+			let ref = panels[i].attr('data-section-name');
+			$('.pagination .active').removeClass('active');
+			$('.pagination').find('a[href="#' + ref + '"]').addClass("active");
+		},
+		afterRender:function(){
+			let pagination = '<ul class="pagination">';
+			let activeClass = '';
+			$('.scroll_block').each(function(i) {//1ページスクロールさせたいエリアクラス名を指定
+			activeClass = '';
+			if(i===$.scrollify.currentIndex()) {
+				activeClass = 'active';
+			}
+			pagination += '<li><a class="' + activeClass + '" href="#' + $(this).attr("data-section-name") + '"></a></li>';
+			});
+			pagination += '</ul>';
+			$('#about').append(pagination);//はじめのエリアにページネーションを表示
+			$('.pagination a').on('click',$.scrollify.move);
+		},
+	});
+
+	$(window).on('resize',function(){
+		if(current){
+			let currentScrl = $('.scroll_block').eq(current).offset().top;
+			$(window).scrollTop(currentScrl);
+		}
 	});
 });
